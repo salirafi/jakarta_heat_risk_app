3bd21fc3b98c127d32cd76f372dc672aceac2662
Rafi <89698848+salirafi@users.noreply.github.com>
2026-03-10 14:46:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from shiny import App
-from ui.layout import app_ui
-from ui.app_server import server
+from app.ui.layout import app_ui
+from app.ui.app_server import server
+
 
 app = App(app_ui, server)
