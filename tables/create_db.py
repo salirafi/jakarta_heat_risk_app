59521d6fc9d3ad4a1283921c222aea54615c3610
Rafi <89698848+salirafi@users.noreply.github.com>
2026-03-11 21:45:02 +0900

    Update create_db.py

--- tables/create_db.py
+++ tables/create_db.py
@@ -7,9 +7,10 @@ DB_PATH = BASE_DIR / "tables" / "heat_risk.db"
 conn = sqlite3.connect(DB_PATH)
 cursor = conn.cursor()
 
-with open("heat_risk.sql", "r") as f:
+with open(BASE_DIR / "tables" / "heat_risk.sql", "r") as f:
     cursor.executescript(f.read())
 
 conn.commit()
 
 conn.close()
+
